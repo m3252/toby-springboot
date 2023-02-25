d0ffc3f6b4a3e44548a9c306ff27db5a050528ce
noah <m3252@naver.com>
2023-02-25 16:57:11 +0900

    feat/refactor: 커스텀 컨디션 추가, jetty build 제거

--- helloboot/src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
+++ helloboot/src/main/java/tobyspring/config/autoconfig/JettyWebServerConfig.java
@@ -8,10 +8,12 @@ import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.util.ClassUtils;
+import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
-@Conditional(JettyWebServerConfig.JettyCondition.class)
+@ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
 
     @Bean("jettyWebServerFactory")
@@ -19,10 +21,4 @@ public class JettyWebServerConfig {
         return new JettyServletWebServerFactory();
     }
 
-    static class JettyCondition implements Condition {
-        @Override
-        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-            return true;
-        }
-    }
 }
