de2f6c60476006fe549893e5af6a9f3f1be5a0e7
noah <m3252@naver.com>
2023-02-25 17:31:46 +0900

    feat/refactor: @ConditionalOnMissingBean 활용하여 tomcat bean 설정 지정하기

---
+++ helloboot/src/main/java/tobyspring/helloboot/WebServerConfiguration.java
@@ -0,0 +1,16 @@
+package tobyspring.helloboot;
+
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class WebServerConfiguration {
+    @Bean
+    ServletWebServerFactory customWebServerFactory() {
+        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+        tomcatServletWebServerFactory.setPort(1111);
+        return tomcatServletWebServerFactory;
+    }
+}
