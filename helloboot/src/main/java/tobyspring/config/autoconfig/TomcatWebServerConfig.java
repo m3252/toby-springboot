33470676e249081c7e1e0374c393099344fb8a51
noah <m3252@naver.com>
2023-02-26 15:09:55 +0900

    refactor: 자동 구성 정보에 Environment 프로퍼티 적용
    설정을 통해 환경 변수 우선순위 확인

--- helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
+++ helloboot/src/main/java/tobyspring/config/autoconfig/TomcatWebServerConfig.java
@@ -5,6 +5,7 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 
+import org.springframework.core.env.Environment;
 import tobyspring.config.ConditionalMyOnClass;
 import tobyspring.config.MyAutoConfiguration;
 
@@ -14,7 +15,9 @@ public class TomcatWebServerConfig {
 
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory servletWebServerFactory(Environment env) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.setContextPath(env.getProperty("contextPath"));
+        return factory;
     }
 }
--- helloboot/src/test/java/tobyspring/helloboot/HelloApiTest.java
+++ helloboot/src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -17,7 +17,7 @@ public class HelloApiTest {
     void helloApi() {
         //http localhost:8080/hello?name=Spring
         ResponseEntity<String> res =
-                rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+                rest.getForEntity("http://localhost:8080/app/hello?name={name}", String.class, "Spring");
 
         // status 200
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -31,7 +31,7 @@ public class HelloApiTest {
     void failsHelloApi() {
         //http localhost:8080/hello?name=
         ResponseEntity<String> res =
-                rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+                rest.getForEntity("http://localhost:8080/app/hello?name=", String.class);
 
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
     }
